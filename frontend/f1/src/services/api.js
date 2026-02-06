460ff3dd3618e72f6abb30cff9c547735918ec34
Shivam kumar <shivam68338@gmail.com>
2026-02-06 22:27:44 +0530

    Update api.js

--- frontend/f1/src/services/api.js
+++ frontend/f1/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://events-3-pcgs.onrender.com",
+  baseURL: "https://events-3-pcgs.onrender.com/api",
   withCredentials: true,
 });
 
